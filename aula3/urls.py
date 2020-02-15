57223a9d84dd576baedf709e4e30edcf9a93faa5
Elias S. S. Vallejos <eliasssv@hotmail.com>
2020-02-15 16:07:57 -0300

    aula 03 GET e POST

--- aula3/urls.py
+++ aula3/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
-from .views import index, seta_cookie, redirect
+from . import views 
 
 app_name = "aula3"
 
 urlpatterns = [
-    path('', index),
-    path('cookie', seta_cookie),
-    path('uol', redirect),
+    path('', views.index),
+    path('cookie', views.seta_cookie),
+    path('uol', views.redirect),
+    path('<int:code>', views.show_code),
+    path('cat/<int:code>', views.show_cat),
+    path('get/', views.show_get_values),
+    path('post/', views.show_post_values),
 ] 
--- aula3/views.py
+++ aula3/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest, HttpResponseRedirect
 from django.utils import timezone
+from django.views.decorators.csrf import csrf_exempt
 
 def index(request):
     html = "<h1>Bem vindo</h1>"
@@ -15,4 +16,35 @@ def seta_cookie(request):
 
 def redirect(request):
     return HttpResponseRedirect("http://uol.com.br")
-    
+
+def show_code(request, code):
+    html = f"<h1>O código é {code}</h1>"
+    response = HttpResponse(html)
+    return response
+
+def show_cat(request, code):
+    return HttpResponseRedirect(f"https://http.cat/{code}")
+
+def show_get_values(request):
+    nome = request.GET.get("nome", "Não informado")
+    html = f"<h1>Bem vindo {nome}</h1>"
+    return HttpResponse(html)
+
+@csrf_exempt
+def show_post_values(request):
+    head =""
+    if request.method=="POST":
+        nome = request.POST.get("nome")
+        sobrenome = request.POST.get("sobrenome")
+        head = f"<h1>Bem vindo {nome} {sobrenome}</h1>"
+
+    html = """
+        <form method=POST>
+            <label for="fname">Nome:</label><br>
+            <input type="text" id="nome" name="nome" value=""><br>
+            <label for="sobrenome">Sobrenome:</label><br>
+            <input type="text" id="sobrenome" name="sobrenome" value=""><br><br>
+            <input type="submit" value="salvar">
+         </form> 
+    """
+    return HttpResponse(head+html)
