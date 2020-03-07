53a089383fdb675cfb358528dcf69ef023f31556
Elias S. S. Vallejos <eliasssv@hotmail.com>
2020-03-07 11:39:45 -0300

    aula 7

---
+++ aula7/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+from .forms import UserLoginForm
+from django.contrib.auth import login, logout
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth.decorators import login_required, permission_required
+
+def index(request):
+    next = request.GET.get("next", "/aula7")
+    form = UserLoginForm()
+    if request.method == "POST":
+        form = UserLoginForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            next = request.POST.get("next", "/aula7")
+            return HttpResponseRedirect(next)
+
+    context = {
+        "form": form,
+        "next": next,
+    }
+    return render(request, 'aula7/index7.html', context=context)
+
+@login_required
+def restrita(request):
+    return HttpResponse("View Restrita")
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect('/aula7')
+
+@permission_required('view_carrinho')
+def permission_view(request):
+    return HttpResponse("view com permissao")
